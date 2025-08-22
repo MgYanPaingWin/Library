a6414181d41aa913b966dc3e48f04437cc949188
Yan Pai Win <yanpaiwin201916@gmail.com>
2025-08-22 22:08:40 +0630

    ep-57

--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useRef } from "react";
 import Navbar from "../../components/Navbar";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+import './style.css';
+
 
 export default function Layout() {
+  const location = useLocation();
+  const nodeRef = useRef(null);
+
   return (
     <div>
-      <Navbar/>
-      
-      {/* router */}
-      <div className="max-w-6xl mx-auto p-3">
-        <Outlet />
-      </div>
+      <Navbar />
+
+      <SwitchTransition>
+        <CSSTransition timeout={200} classNames='fade' nodeRef={nodeRef} key={location.pathname} >
+          <div ref={nodeRef} className="max-w-6xl mx-auto p-3">
+            <Outlet />
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   );
 }
