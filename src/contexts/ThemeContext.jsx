e3259380824c7fac3550510aeeac238f140f1a83
Yan Pai Win <yanpaiwin201916@gmail.com>
2025-11-13 10:02:36 +0630

    create context and export

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,13 @@
+//themecontext
+import { createContext } from "react";
+
+const ThemeContext = createContext();
+
+//themecontextprovider component
+const ThemeContextProvider = ({ children }) => {
+  return <ThemeContext.Provider value={{theme : 'dark'}}>
+    {children}
+  </ThemeContext.Provider>;
+};
+
+export {ThemeContext,ThemeContextProvider}
